7c667680b2c2635e37da368e3c20565df51f7096
w16-cfr\Gabor Bata Kovacs <gabor.batakovacs@castlefrog.fr>
2019-08-26 23:12:37 +0200

    DB Changes

---
+++ UrbanWarming/UW.DataAccess/DAL/UWDataAccess.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UW.DataAccess.DAL
+{
+    public partial class UWDataAccess : UrbanWarmingContext
+    {
+        public UWDataAccess() : base()
+        { }
+    }
+}
---
+++ UrbanWarming/UW.DataAccess/DbContext/UrbanWarmingContext.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace UW.DataAccess
+{
+    public class UrbanWarmingContext
+    {
+        protected string ConnectionString { get; set; }
+
+        public UrbanWarmingContext()
+        {
+            //var appSettingsJson = AppSettingsJson.GetAppSettings();
+            //this.ConnectionString = appSettingsJson["MssqlConnectionString"];
+            this.ConnectionString = "Server=museiondbserver.database.windows.net;Database=MuseionDatabase;User Id=museionadmin;Password=fdsf/%/=6niu;Pooling=true";
+        }
+
+        public UrbanWarmingContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(this.ConnectionString);
+            if (connection.State != ConnectionState.Open)
+                connection.Open();
+            return connection;
+        }
+
+        protected DbCommand GetCommand(DbConnection connection, string commandText, CommandType commandType)
+        {
+            SqlCommand command = new SqlCommand(commandText, connection as SqlConnection);
+            command.CommandType = commandType;
+            return command;
+        }
+
+        protected SqlParameter GetParameter(string parameter, object value)
+        {
+            SqlParameter parameterObject = new SqlParameter(parameter, value != null ? value : DBNull.Value);
+            parameterObject.Direction = ParameterDirection.Input;
+            return parameterObject;
+        }
+
+        protected SqlParameter GetParameterOut(string parameter, SqlDbType type, object value = null, ParameterDirection parameterDirection = ParameterDirection.InputOutput)
+        {
+            SqlParameter parameterObject = new SqlParameter(parameter, type); ;
+
+            if (type == SqlDbType.NVarChar || type == SqlDbType.VarChar || type == SqlDbType.NText || type == SqlDbType.Text)
+            {
+                parameterObject.Size = -1;
+            }
+
+            parameterObject.Direction = parameterDirection;
+
+            if (value != null)
+            {
+                parameterObject.Value = value;
+            }
+            else
+            {
+                parameterObject.Value = DBNull.Value;
+            }
+
+            return parameterObject;
+        }
+
+        protected int ExecuteNonQuery(string procedureName, List<DbParameter> parameters, CommandType commandType = CommandType.StoredProcedure)
+        {
+            int returnValue = -1;
+
+            try
+            {
+                using (SqlConnection connection = this.GetConnection())
+                {
+                    DbCommand cmd = this.GetCommand(connection, procedureName, commandType);
+
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    returnValue = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return returnValue;
+        }
+
+        protected object ExecuteScalar(string procedureName, List<DbParameter> parameters)
+        {
+            object returnValue = null;
+
+            try
+            {
+                using (DbConnection connection = this.GetConnection())
+                {
+                    DbCommand cmd = this.GetCommand(connection, procedureName, CommandType.StoredProcedure);
+
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    returnValue = cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return returnValue;
+        }
+
+        protected object ExecuteScalar(string procedureName, List<SqlParameter> parameters)
+        {
+            object returnValue = null;
+
+            try
+            {
+                using (DbConnection connection = this.GetConnection())
+                {
+                    DbCommand cmd = this.GetCommand(connection, procedureName, CommandType.StoredProcedure);
+
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+                    returnValue = cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return returnValue;
+        }
+
+        protected DataTable GetDataTable(string procedureName, List<SqlParameter> parameters)
+        {
+            DataTable returnValue = new DataTable();
+
+            try
+            {
+                using (var con = GetConnection())
+                using (var cmd = new SqlCommand(procedureName, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+                    da.Fill(returnValue);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return returnValue;
+        }
+
+        protected DbDataReader GetDataReader(string procedureName, List<DbParameter> parameters, CommandType commandType = CommandType.StoredProcedure)
+        {
+            DbDataReader ds;
+
+            try
+            {
+                DbConnection connection = this.GetConnection();
+                {
+                    DbCommand cmd = this.GetCommand(connection, procedureName, commandType);
+                    if (parameters != null && parameters.Count > 0)
+                    {
+                        cmd.Parameters.AddRange(parameters.ToArray());
+                    }
+
+                    ds = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return ds;
+        }
+
+    }
+}

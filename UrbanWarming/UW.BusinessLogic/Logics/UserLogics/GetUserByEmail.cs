0e49bc8c61cff15a5a6f1aaff4adeaf112b16893
Martin Morvai <morvaimartin91@gmail.com>
2019-09-03 21:56:14 +0200

    Get user by email

---
+++ UrbanWarming/UW.BusinessLogic/Logics/UserLogics/GetUserByEmail.cs
@@ -0,0 +1,21 @@
+﻿using UW.BusinessLogic.DTOs;
+using UW.DataAccess.DAL;
+
+namespace UW.BusinessLogic.Logics
+{
+    public partial class UserLogics
+    {
+        public static UserDTO GetUserByEmail(string email)
+        {
+            try
+            {
+                UWDataAccess dataAccess = new UWDataAccess();
+                return new UserDTO(dataAccess.GetUserByEmail(email));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ UrbanWarming/UW.DataAccess/DAL/Get/GetUserByEmail.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using UW.DataAccess.Entities;
+
+namespace UW.DataAccess.DAL
+{
+    public partial class UWDataAccess
+    {
+        public User GetUserByEmail(string email)
+        {
+            try
+            {
+                List<SqlParameter> parameters = new List<SqlParameter>();
+                parameters.Add(base.GetParameter("Email", email));
+
+                DataTable dt = base.GetDataTable("sp_GetUserByEmail", parameters);
+
+                return GetUserFromDataTable(dt);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}

fb4df3e9c6a3363b05827a32d8219cbd915c2fbe
Martin Morvai <morvaimartin91@gmail.com>
2019-09-04 21:34:36 +0200

    Get city types

---
+++ UrbanWarming/UW.BusinessLogic/Logics/CityTypeLogics/GetCityTypes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UW.BusinessLogic.DTOs;
+using UW.DataAccess.DAL;
+
+namespace UW.BusinessLogic.Logics
+{
+    public class CityTypeLogic
+    {
+        public static IEnumerable<CityTypeDTO> GetCityTypes()
+        {
+            try
+            {
+                UWDataAccess da = new UWDataAccess();
+
+                return da.GetCityTypes().Select(t => new CityTypeDTO(t));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ UrbanWarming/UW.DataAccess/DAL/Get/GetCityTypes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using UW.DataAccess.Entities;
+
+namespace UW.DataAccess.DAL
+{
+    public partial class UWDataAccess
+    {
+        public IEnumerable<CityType> GetCityTypes()
+        {
+            try
+            {
+                DataTable dt = base.GetDataTable("sp_GetCityTypes", null);
+
+                return GetCityTypesFromDataTable(dt);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private IEnumerable<CityType> GetCityTypesFromDataTable(DataTable dt)
+        {
+            List<CityType> cityTypes = new List<CityType>();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                CityType type = new CityType(
+                        row["Id"].ToString(),
+                        row["Name"].ToString(),
+                        row["OfficialName"].ToString(),
+                        row["FictionalName"].ToString(),
+                        row["Region"].ToString(),
+                        Convert.ToBoolean(row["SeaConnection"].ToString()),
+                        Convert.ToBoolean(row["RoadConnection"].ToString()),
+                        row["Difficulty"].ToString(),
+                        row["Description"].ToString()
+                    );
+
+                cityTypes.Add(type);
+            }
+
+            return cityTypes;
+        }
+
+    }
+}

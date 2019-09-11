fac5737999fa79290127c2602177ec868551af24
Martin Morvai <morvaimartin91@gmail.com>
2019-09-11 22:42:22 +0200

    Delete user unit test

---
+++ UrbanWarming/UW.BusinessLogic.Test/Logics/UserLogics/DeleteUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UW.BusinessLogic.DTOs;
+using UW.BusinessLogic.Logics;
+
+namespace UW.BusinessLogic.Test
+{
+    [TestClass]
+    public class DeleteUser
+    {
+
+        [TestMethod]
+        public void DeleteUser_Success()
+        {
+            string id = DateTime.Now.ToString();
+            UserDTO user = new UserDTO(id, "willbedeleted", "email", "pic", "i will be deleted soon", 1);
+            UserLogics.InsertUser(user);
+
+            bool deleteResult = UserLogics.DeleteUser(id);
+
+            Assert.AreEqual(true, deleteResult);
+        }
+
+        [TestMethod]
+        public void DeleteUser_UserNotExists()
+        {
+            string id = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+            bool deleteResult = UserLogics.DeleteUser(id);
+
+            Assert.AreEqual(true, deleteResult);
+        }
+    }
+}

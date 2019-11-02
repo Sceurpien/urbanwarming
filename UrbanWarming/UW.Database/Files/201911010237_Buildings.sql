acbf9b1f54ce69f9d729525f3190ae3477bde977
w16-cfr\Gabor Bata Kovacs <gabor.batakovacs@castlefrog.fr>
2019-11-02 15:34:58 +0100

    Buildings

--- UrbanWarming/UW.Database/Files/201911010237_Buildings.sql
+++ UrbanWarming/UW.Database/Files/201911010237_Buildings.sql
@@ -7,7 +7,8 @@ CREATE TABLE [dbo].[Building](
 [Id] [int] IDENTITY NOT NULL,
 [CityId] [int] NOT NULL,
 [CityMapId] [int] NULL,
-[BuldingTypeId] [int] NOT NULL,
+[BuildingTypeId] [int] NOT NULL,
+[BuildingClassId] [int] NOT NULL,
 [Name] [nvarchar](1000) NULL,
 [Level] [float] NOT NULL DEFAULT 0,
 [Visitor] [int] NOT NULL DEFAULT 0,
@@ -23,8 +24,9 @@ PRIMARY KEY CLUSTERED
 GO
 
 CREATE TABLE [dbo].[BuildingType](
-[Id] [int] IDENTITY NOT NULL,
+[Id] [int] NOT NULL,
 [Name] [nvarchar](1000) NULL,
+[BuildingClassId] [int] NOT NULL,
 [BasePrice] [float] NOT NULL DEFAULT 0,
 [BaseMaintenance] [float] NOT NULL DEFAULT 0
 PRIMARY KEY CLUSTERED 
@@ -32,4 +34,32 @@ PRIMARY KEY CLUSTERED
 [Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY];
+GO
+
+CREATE TABLE [dbo].[BuildingClass](
+[Id] [int] NOT NULL,
+[Name] [nvarchar](1000) NULL
+PRIMARY KEY CLUSTERED 
+(
+[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY];
+GO
+
+INSERT INTO  [dbo].[BuildingClass] (Id,Name)
+VALUES	(1,'Healthcare'),
+		(2,'Education'),
+		(3,'Social Services'),
+		(4,'Powerplant'),
+		(5,'Transportation');
+GO
+
+INSERT INTO  [dbo].[BuildingType] (Id,Name,BuildingClassId,BasePrice,BaseMaintenance)
+VALUES	(1,'Doctor Office',1,1,1),
+		(2,'Hospital',1,1,1),
+		(3,'School',2,1,1),
+		(4,'Lyceum',2,1,1),
+		(5,'Firestation',3,1,1),
+		(6,'Police station',3,1,1)
+		;
 GO
